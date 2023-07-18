b27babcae0ad1842750496e45a0a6b6f6a9a2d7d
Krishna-7777 <kchhabraji13@gmail.com>
2023-07-18 16:51:54 +0530

    added try catch block

--- backend/routes/videoRouter.js
+++ backend/routes/videoRouter.js
@@ -17,13 +17,18 @@ function extractData(item) {
 setInterval(async () => {
     let date = new Date(new Date().getMinutes() - 30).toISOString()
     setTimeout(async () => {
-        let data = await fetch(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=15&order=date&publishedAfter=${date}&safeSearch=moderate&type=video&key=${process.env.key}`);
+        try {
+            let data = await fetch(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=15&order=date&publishedAfter=${date}&safeSearch=moderate&type=video&key=${process.env.key}`);
         data = await data.json()
         if (data.items.length) {
             const extractedData = data.items.map((item) => extractData(item));
             await VideoModel.insertMany(extractedData)
             console.log("data inserted")
         }
+        } catch (error) {
+            console.log(error)
+        }
+        
     }, 30000)
 }, 30000)
 
